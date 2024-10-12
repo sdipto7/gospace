6a858b2e5e6262bc003ed368b66176c68352526c
Shahriar Rumi Dipto <shahriardipto7@gmail.com>
2024-10-12 23:04:37 +0600

    Fixed destination data missing issue

--- exploration/src/main/resources/data.sql
+++ exploration/src/main/resources/data.sql
@@ -1,3 +1,3 @@
 INSERT IGNORE INTO destination VALUES
-(1, 0, NOW(), NULL, 'Mars', 'PLANET', 'Description of planet Mars', 'Surface Features of planet Mars',
-       'Atmosphere composition of Mars', 2.114114, 6779.00, 639000000000000000000000.00, 3.71, -153.00, 20.00);
+(1, 0, NOW(), NULL, 'Mars', 'PLANET', 'Description of planet Mars', 'Surface Features of planet Mars', 'Atmosphere composition of Mars', 2.114114, 6779.00, 639000000000000000000000.00, 3.71, -153.00, 20.00),
+(2, 0, NOW(), NULL, 'Titan', 'MOON', 'It is one of the 82 moons of Planet Saturn', 'Surface Features of Titan', 'Atmosphere composition of Titan', 8.021504546722, 5149.5, 135000000000000000000000.00, 1.352, -179.6, -179.6);
